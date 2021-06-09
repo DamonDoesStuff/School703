8eba67133026a5f0c547facdfae1f07c5a19b84f
Damon Rochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-09 14:50:58 +0000

    spaces

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.getElementById('button').addEventListener('click', comparison)
 function comparison () {
   age = document.getElementById('hello').value
   if (age >= hello) {
-    alert('You can Watch R rated movies alone') 
-    } else if (age <= hello) {
-    alert('You can Watch PG-13, PG, or G rated movie alone')
+    alert('You can Watch R rated movies alone')
+  } else if (age <= hello) {
+    alert('You can Watch PG-13, PG, or G rated movies alone')
   }
 }
